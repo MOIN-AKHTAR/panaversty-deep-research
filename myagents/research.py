0ec7e1744ca10556285cf2a469fd3e75067fa181
MOIN-AKHTAR <moinakhter179@gmail.com>
2025-09-04 02:08:12 +0500

    Added model settings

--- myagents/research.py
+++ myagents/research.py
@@ -3,11 +3,17 @@ from prompts.research import research_agent_prompt
 from tools.websearch import web_search
 from .citation import citation_agent
 from .reflection import reflection_agent
+from agents import ModelSettings
 
 
 research_agent = base_agent.clone(
     name="ResearchAgent", 
     instructions=research_agent_prompt, 
+    model_settings = ModelSettings(
+        max_tokens=1000,
+        parallel_tool_calls=True,
+        temperature=0.3,
+    ),
     tools=[ 
         web_search, 
         citation_agent.as_tool(tool_name="CitationAgent",tool_description="Generates proper academic citations and ensures all research sources are properly attributed with consistent formatting."),
